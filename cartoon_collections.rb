39d149060666b1ab95d0fc798c0a747af1a1a5b6
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-30 07:38:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.find?(){|el| el
+  array.find?(){|el| el = "cheese"}
 end
