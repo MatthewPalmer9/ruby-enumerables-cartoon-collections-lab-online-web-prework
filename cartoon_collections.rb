d992a92d897c8cb10f0e4f05f5c686c90eb20ed2
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-30 07:04:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(array)
   array.map(){|el| el.capitalize}
-  array.map(){|el| el + "!"
+  array.map(){|el| el + "!"}
 end
 
 def long_planeteer_calls# code an argument here
