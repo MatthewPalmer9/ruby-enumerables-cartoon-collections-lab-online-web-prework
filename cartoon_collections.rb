1bc94f3bcb67b49c5483105b61ee3ab56a04ea49
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-30 07:30:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.reduce(){ |el| p el.length > 4}
+  
+  array.reduce(){|el|}
 end
 
 def find_the_cheese# code an argument here
